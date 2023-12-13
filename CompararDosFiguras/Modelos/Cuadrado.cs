4cf4e8e5acde7dee12027940721ab77dfac82b31
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-12 22:01:32 -0600

    Se pueden comparar cuadrados entre si

--- CompararDosFiguras/Modelos/Cuadrado.cs
+++ CompararDosFiguras/Modelos/Cuadrado.cs
@@ -3,7 +3,7 @@ using CompararDosFiguras.Interfaces;
 
 namespace CompararDosFiguras.Modelos
 {
-    public class Cuadrado : IFigura
+    public class Cuadrado : IFigura, IComparable
     {
         public double Lado { get; set; }
 
@@ -31,6 +31,33 @@ namespace CompararDosFiguras.Modelos
         public Cuadrado(double lado)
         {
             Lado = lado;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(obj == null)
+            {
+                return 1;
+            }
+
+            var cuadradoRecibido = obj as Cuadrado;
+            if(cuadradoRecibido == null)
+            {
+                throw new Exception("El objeto pasado no es un cuadrado");
+            }
+
+            if(this.Area > cuadradoRecibido.Area)
+            {
+                return 1; //Indica que nuestro objeto es mayor
+            }
+            else if(this.Area == cuadradoRecibido.Area)
+            {
+                return 0; //Indica que ambos objetos son iguales en valor
+            }
+            else
+            {
+                return -1; //Indica que nuestro objeto es menor que el recibido
+            }
         }
     }
 }
